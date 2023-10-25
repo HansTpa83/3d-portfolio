bf03da678469f6d67c203da639ca30497230bd35
Hans <hanstapia83@gmail.com>
2023-10-25 22:58:17 +0200

    WIP style

--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import jsonResume from "../../resume.json"
+import "../assets/style/project.scss"
 
 export default function Projects() {
     const [projects, setProjects] = useState(jsonResume.projects)
     return (
 
         <div style={{
-            backgroundColor: "red",
             width: "945px",
             height: "755px",
             overflow: "auto"
@@ -14,14 +14,28 @@ export default function Projects() {
             {
                 projects.map((project, index) => {
                     const { imagePreview } = jsonResume.meta.projects[project.name]
-                    return (
-                        <div key={index} style={{ margin: "90px auto", width: "450px", height: "auto", borderRadius: "20px", textAlign: "center", backgroundColor: "white" }}>
+                    return index % 2 == 0 ? (
+                        <div key={index} style={{ display: "flex", justifyContent: "space-around", margin: "90px auto", width: "90%", height: "auto", borderRadius: "20px", textAlign: "center" }}>
+                            <div style={{ color: "white" }}>
+                                <div style={{ fontSize: "3em" }}> {project.name} </div>
+                                <div>{project.description}</div>
+                            </div>
                             <div style={{
                                 width: "450px", height: "450px", backgroundImage: `url(${imagePreview})`, borderTopRightRadius: "20px", borderTopLeftRadius: "20px"
                             }}></div>
-                            < div style={{ fontSize: "3em" }}> {project.name} </div>
                         </div>
-                    )
+                    ) :
+                        (
+                            <div key={index} style={{ display: "flex", justifyContent: "space-around", margin: "90px auto", width: "90%", height: "auto", borderRadius: "20px", textAlign: "center" }}>
+                                <div style={{
+                                    width: "450px", height: "450px", backgroundImage: `url(${imagePreview})`, borderTopRightRadius: "20px", borderTopLeftRadius: "20px"
+                                }}></div>
+                                <div style={{ color: "white" }}>
+                                    <div style={{ fontSize: "3em" }}> {project.name} </div>
+                                    <div>{project.description}</div>
+                                </div>
+                            </div>
+                        )
                 })
             }
         </div >

1f31286ac9607dacb7a664bd7578036cf8af1a1a
Hans <hanstapia83@gmail.com>
2023-10-29 23:10:30 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
+import gsap from 'gsap';
 import { useCameraStore } from "../store/store"
+import React, { useState, useEffect } from 'react'
+import { MdHome, MdOutlineHome, MdNavigateNext, MdNavigateBefore } from 'react-icons/md';
 
 export default function Navbar(props) {
     const camera = useCameraStore(state => state.cameraName)
     const switchCamera = useCameraStore((state) => state.switchCamera)
 
-    console.log(camera);
+    const steps = [
+        'home',
+        // 'presentation',
+        'project',
+        // 'education',
+        'experiences',
+        'contact'
+    ]
+
+    const [activeStep, setActiveStep] = useState(0);
+
+    const stepTranslation = {
+        'home': "Accueil",
+        'presentation': "Présentation",
+        'project': "Projets",
+        'education': "Formation",
+        'experiences': "Expérience",
+        'contact': "Contact"
+    }
+
+    const handleClick = (type) => {
+        let index = steps.indexOf(camera)
+        const name = type == "next" ? steps[index + 1] : steps[index - 1]
+        const newIndex = type == "next" ? index + 1 : index - 1
+        if (newIndex >= steps.length || newIndex < 0) return
+        setActiveStep(newIndex)
+        switchCamera(name)
+    }
 
     return (
-        <nav style={{ backgroundColor: "orange", position: 'absolute', bottom: '10px', right: "50%" }}>
-            <ul>
-                <li onClick={() => switchCamera("home")}>Accueil</li>
-                {/* <li onClick={() => switchCamera()}>Formations</li> */}
-                <li onClick={() => switchCamera("experiences")}>Expériences</li>
-                <li onClick={() => switchCamera("skills")}>Skills</li>
-                <li onClick={() => switchCamera("contact")}>Contact</li>
-            </ul>
-        </nav>
+        <div className='interface-controls' style={{ backgroundColor: "orange", position: 'absolute', bottom: '10px', right: "50%", display: 'flex', width: "10rem", borderRadius: "10px", height: "4vh", alignItems: 'center' }}>
+            <button onClick={() => handleClick("previous")}>
+                <MdNavigateBefore />
+            </button >
+            <div style={{ width: '10rem', textAlign: "center" }}>
+                {stepTranslation[steps[activeStep]]}
+            </div>
+            <button onClick={() => handleClick("next")} >
+                <MdNavigateNext />
+            </button>
+        </div>
+
     )
 }

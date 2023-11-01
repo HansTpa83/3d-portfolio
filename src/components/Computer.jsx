b097330aa4706f7163882c703c6a0b8f214c7bab
Hans <hanstapia83@gmail.com>
2023-11-01 22:45:42 +0100

    fix computer size+ add style

--- src/components/Computer.jsx
+++ src/components/Computer.jsx
@@ -1,31 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { useGLTF, Html, TransformControls } from '@react-three/drei'
 import * as THREE from 'three'
-
+import React, { useRef } from 'react'
+import { useCameraStore } from "../store/store"
+import { useGLTF, Html } from '@react-three/drei'
 
 import Projects from '../page/Projects'
-
 
 export default function Computer() {
     const { scene } = useGLTF("/models/computer.gltf")
     const html = useRef(null)
+
+    const camera = useCameraStore(state => state.cameraName)
+    const cameraMoving = useCameraStore(state => state.isCameraMoving)
 
     const quaternion = new THREE.Quaternion()
     quaternion.setFromAxisAngle(new THREE.Vector3(-.1, 2, .1).normalize(), Math.PI / 2)
 
     return (
         <primitive object={scene} >
+            {camera === "projects" && !cameraMoving &&
+                <Html
+                    ref={html}
+                    transform
+                    wrapperClass="htmlScreen"
+                    distanceFactor={.27}
+                    position={[-.04, 2.196, .055]}
+                    quaternion={quaternion}
+                >
+                    <Projects />
+                </Html>
 
-            <Html
-                ref={html}
-                transform
-                wrapperClass="htmlScreen"
-                distanceFactor={.27}
-                position={[-.04, 2.205, .05]}
-                quaternion={quaternion}
-            >
-                <Projects />
-            </Html>
+            }
         </primitive>
     )
 }

0b1c253d5349ff25a70e3e42b89d2ecf284ebd34
Hans <hanstapia83@gmail.com>
2023-11-02 20:01:12 +0100

    fix positions

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -30,28 +30,28 @@ export default function Dashboard() {
 
             <primitive object={bluePostItClone} position={[0.01, 0.25, 2.4]}>
                 {camera === "experiences" && !cameraMoving &&
-                    <Html distanceFactor={9} transform position={[-8.7, 5.8, 0.9]} rotation={[0, 1.57, 0]}>
+                    <Html distanceFactor={9} transform position={[-8.7, 5.75, 0.9]} rotation={[0, 1.57, 0]}>
                         {companyName(resume.work[0].name)}
                     </Html>
                 }
             </primitive>
             <primitive object={bluePostIt} position={[0.01, 0.25, 1.8]}>
                 {camera === "experiences" && !cameraMoving &&
-                    <Html distanceFactor={3.8} transform position={[-5.5, 4.6, -1.2]} rotation={[0, 1.57, 0]}>
+                    <Html distanceFactor={3.8} transform position={[-5.5, 4.55, -1.2]} rotation={[0, 1.57, 0]}>
                         {position(resume.work[0].position)}
                     </Html>
                 }
             </primitive>
             <primitive object={pinkPostIt} position={[0.01, 0.55, 1.48]}>
                 {camera === "experiences" && !cameraMoving &&
-                    <Html distanceFactor={4.5} transform position={[-5.9, 4.4, -1.8]} rotation={[0, 1.57, 0]}>
+                    <Html distanceFactor={4.5} transform position={[-5.9, 4.35, -1.8]} rotation={[0, 1.57, 0]}>
                         {date(resume.work[0].startDate)}
                     </Html>
                 }
             </primitive>
             <primitive object={pinkPostItClone1} position={[0.01, 0.55, 1.1]}>
                 {camera === "experiences" && !cameraMoving &&
-                    <Html distanceFactor={4.5} transform position={[-5.9, 4.4, -2.13]} rotation={[0, 1.57, 0]}>
+                    <Html distanceFactor={4.5} transform position={[-5.9, 4.35, -2.13]} rotation={[0, 1.57, 0]}>
                         {date(resume.work[0].endDate)}
                     </Html>
                 }
